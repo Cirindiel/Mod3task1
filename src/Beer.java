c0733165afe5cdfbb4d018a178beb6e69fcc98a0
Piotr Zadroga <pmjzadroga@gmail.com>
2020-10-09 21:25:16 +0200

    Dodanie konstruktora.

--- src/Beer.java
+++ src/Beer.java
@@ -1,6 +1,13 @@
-class Beer {
+public class Beer {
     String name;
     String type;
     double voltage;
     int amount;
+
+    Beer(String n, String t, double v, int a) {
+    name = n;
+    type = t;
+    voltage = v;
+    amount = a;
+    }
 }
--- src/Brewery.java
+++ src/Brewery.java
@@ -1,22 +1,13 @@
 public class Brewery {
     public static void main(String[] args) {
-        Beer beer1 = new Beer();
-        beer1.name = "1 na 100";
-        beer1.type = "Pale Ale";
-        beer1.voltage = 1;
-        beer1.amount = 500;
-
-        Beer beer2 = new Beer();
-        beer2.name = "Waszczukowe Polish Pale Ale";
-        beer2.type = "Pale Ale";
-        beer2.voltage = 4.9;
-        beer2.amount = 500;
+        Beer beer1 = new Beer("1 na 100", "Pale Ale", 1, 500);
+        Beer beer2 = new Beer("Waszczukowe Polish Pale Ale", "Pale Ale", 4.9, 500);
 
         System.out.println("Nazwa piwa: " + beer1.name);
         System.out.println("Rodzaj: " + beer1.type);
         System.out.println("Woltaż: " + beer1.voltage);
         System.out.println("Objętość: " + beer1.amount);
-        
+
         System.out.println("Nazwa piwa: " + beer2.name);
         System.out.println("Rodzaj: " + beer2.type);
         System.out.println("Woltaż: " + beer2.voltage);
